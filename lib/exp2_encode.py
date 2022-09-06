970bb9e57c9cd35f465fd5ca915bcab0be4e35b5
igordsm83 <engigordantas@gmail.com>
2022-09-06 10:10:41 -0300

    improvement

--- lib/exp2_encode.py
+++ lib/exp2_encode.py
@@ -7,43 +7,6 @@ Created on Mon Sep  5 13:22:38 2022
 """
 import numpy as np 
 
-def get_msb (x):
-    number = x
-    bitpos = 0
-    while number != 0:
-        bitpos+=1             # increment the bit position
-        number = number >> 1 # shift the whole thing to the right once
-    return bitpos
-
-def exp2_encode_vec (X, encods, ratio):
-    
-                    
-    Y = np.zeros((1,0), dtype=int)
-    # Find the most significant bits and replace
-    for i in range (len(X)):
-        msb = int(get_msb (X[i])/ratio)
-        # print('MSB', msb)
-        Y = np.hstack([Y, encods[msb]])
-        
-    return Y[0]
-
-def exp2_encode__test (X, ibits, obits):
-    ratio = int(ibits/obits)
-    # Build the encoded sequences    
-    encods = []
-    for i in range (obits+1):
-        v = np.hstack([np.ones((1,i)), np.zeros((1,obits-i))])
-        encods.append(v.astype(int)) 
-    
-    # Encode sample by sample
-    Y = np.zeros((X.shape[0], obits*X.shape[1]), dtype=int)
-    for i in range(X.shape[0]):
-        vec = np.squeeze(X[i,:])
-        Y[i,:] = exp2_encode_vec (vec, encods, ratio)
-        
-    return Y
-
-
 def exp2_encode (X, ibits, obits):
     ratio = int(ibits/obits)
     
