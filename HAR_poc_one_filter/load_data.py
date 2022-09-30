3f50888319368c8bf7fe24f2becc337138f983ce
mugdhaj17 <97685043+mugdhaj17@users.noreply.github.com>
2022-09-30 13:50:39 -0500

    Add files via upload

---
+++ HAR_poc_one_filter/load_data.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 20:11:08 2022
+
+@author: igor
+"""
+from project_tools import wisard_data_encode, mnist_data_encode_b, mnist_data_encode_t, mnist_data_encode_z, mnist_data_noencode
+from keras.datasets import mnist
+import numpy as np
+from data_augment import gen_data_raw
+from binarization import exponential_thermometer
+from exp2_encode import exp2_encode
+from pandas import read_csv
+from numpy import dstack
+from sklearn.model_selection import train_test_split
+import pickle
+
+def load_data (config):
+    ADDRESS_SIZE = config['ADDRESS_SIZE']
+    THERMO_RESOLUTION = config['THERMO_RESOLUTION']
+    DO_HAMMING = config['DO_HAMMING']
+    DO_AUGMENTATION = config['DO_AUGMENTATION']
+    AUGMENT_RATIO = config['AUGMENT_RATIO']
+
+    N_TRAIN = config['N_TRAIN']
+    N_VAL = config['N_VAL']
+    N_TEST = config['N_TEST']
+    N_TRAIN -= N_VAL
+    
+    # Import from WNN dataset
+    with open('./WNN_dataset/'+'trainX.pkl', 'rb') as inp:
+        X_train = pickle.load(inp)
+    with open('./WNN_dataset/'+'trainy.pkl', 'rb') as inp:
+        Y_train = pickle.load(inp)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.30)
+
+    # power of 2 for samples
+    X_train = np.power(X_train,2)
+    X_test = np.power(X_test, 2)
+
+    # reshaping
+    Y_train = Y_train.reshape(-1)
+    Y_test = Y_test.reshape(-1)
+    
+    # scaling
+    bits_in = 12
+    X_train *= 2**bits_in - 1
+    X_test *= 2**bits_in - 1
+
+    # converting float to int
+    X_train = X_train.astype(int)
+    X_test = X_test.astype(int)
+    
+    if DO_AUGMENTATION:
+        batch_size = 32
+        N_TRAIN = int(X_train.shape[0]*AUGMENT_RATIO)       
+        X_train, Y_train = gen_data_raw (N_TRAIN, batch_size)
+        
+    # Shuffle train set
+    n_shuffled = np.arange(X_train.shape[0])
+    np.random.shuffle(n_shuffled)
+    X_train = X_train[n_shuffled,:,:]
+    Y_train = Y_train[n_shuffled]
+
+    if N_TRAIN>0:
+        # Split data according to configuration
+        X_val = X_train[0:N_VAL,:]
+        Y_val = Y_train[0:N_VAL]
+        n_train_a = len(Y_train)-N_VAL
+        n_train_a = n_train_a if N_TRAIN==-1 else min(N_TRAIN, n_train_a)
+        X_train = X_train[N_VAL:N_VAL+n_train_a,:]
+        Y_train = Y_train[N_VAL:N_VAL+n_train_a]    
+  
+        print('>>> Encoding train set...')
+        X_train_lst = mnist_data_encode_t(X_train, 0,2**bits_in - 1,THERMO_RESOLUTION)
+        # X_train_lst = exp2_encode(X_train.reshape(X_train.shape[0],-1), bits_in, THERMO_RESOLUTION)
+        
+        X_train_lst = X_train_lst.astype(int)
+        Y_train = Y_train.astype(int)
+        
+        if N_VAL>0:
+            print('>>> Encoding val set...')
+            X_val_lst = mnist_data_encode_t(X_val, 0,2**bits_in - 1,THERMO_RESOLUTION)
+            # X_val_lst = exp2_encode(X_val.reshape(X_val.shape[0],-1), bits_in, THERMO_RESOLUTION)
+            
+            X_val_lst = X_val_lst.astype(int)
+            Y_val = Y_val.astype(int)
+        else:
+            X_val_lst = []
+            Y_val = []
+            
+    else:
+        X_train_lst = []
+        Y_train = []
+        X_val_lst = []
+        Y_val = []
+    
+    
+    n_test_a = len(Y_test) if N_TEST==-1 else min(N_TEST, len(Y_test))
+    X_test = X_test[0:n_test_a,:]
+    Y_test = Y_test[0:n_test_a]    
+    print('>>> Encoding test set...')
+    X_test_lst = mnist_data_encode_t(X_test, 0,2**bits_in - 1,THERMO_RESOLUTION)
+    # X_test_lst = exp2_encode(X_test.reshape(X_test.shape[0],-1), bits_in, THERMO_RESOLUTION)
+    
+    X_test_lst = X_test_lst.astype(int)
+    Y_test = Y_test.astype(int)
+
+    return X_train_lst, Y_train, X_val_lst, Y_val, X_test_lst, Y_test
---
+++ HAR_poc_one_filter/run_ensemble.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+COIN (COmbinational Intelligent Networks)
+Ensemble Example
+
+This examples shows how to build an ensemble model for MNIST 
+classification. For this we generate 4 LogicWiSARD models with
+different configurations, as detailed below.
+Most configurations are set in mnist/config.py (see for details).
+Models, results and logs are sent to mnist/out
+"""
+
+import sys
+sys.path.insert(0, '../lib/')
+import os
+from load_config import load_config
+from gen_logicwisard import gen_logicwisard
+from train_coin import train_coin
+from eval_ensemble import eval_ensemble
+import numpy as np
+
+# Sets project name
+project_name = 'mnist'   
+
+# Loads all configurations from config.py file in project dir
+config = load_config('./')
+np.random.seed(config['SEED'])
+
+# Cleans the output directory
+os.system('rm -f ./out/*')
+
+# For each of the 2 configurations, generates 3 LogicWiSARD models, 
+# picking up the 2 the most accurate. 4 models in total
+config['N_SEL_MODELS'] = 2
+config['ADDRESS_SIZE'] = 14
+gen_logicwisard(project_name, config)
+config['ADDRESS_SIZE'] = 16
+gen_logicwisard(project_name, config)
+
+# Convert all LogicWiSARD models to BNN, train them, 
+# and then convert to COINs
+train_coin(project_name, config)
+
+# Evaluate the for models as an ensemble
+eval_ensemble(project_name, config) 
